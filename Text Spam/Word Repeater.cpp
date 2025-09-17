a9827284de5d814e6b310bc728de9beca4ecbcf3
Mostafa_Ahmed_Aboelella <abouelellamostafa811@gmail.com>
2025-09-17 10:31:46 +0300

    Add files via upload

---
+++ Text Spam/Word Repeater.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+void spam(string word , int num , int delay)
+{
+ for (int i = 0; i < num ; i++)
+ {
+   cout<<word<<endl;
+   Sleep(delay);
+ }
+ cout<<"We wrote \""<<word<<"\" , \""<<num<<"\" times "<<" , "<<"with delay \""<<delay<<"\""<<endl;
+ cout<<"The End !";
+}
+int main()
+{
+string word;
+int num ;  
+int delay;
+
+cout<<"Welcome to text spam app \n";
+cout<<"==================================\n";
+cout<<"Enter the word you wanna spam : ";
+getline(cin,word);
+cout<<"\n";
+cout<<"Enter how many times you want spam it : ";
+cin>>num;
+cin.ignore();
+cout<<"\n";
+cout<<"Enter the delay between the word and athor (1000 = 1sec) : ";
+cin>>delay;
+cin.ignore();
+
+cout<<"\n";
+spam(word,num,delay);
+
+}
